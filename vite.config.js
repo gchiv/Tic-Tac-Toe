31b9d0947379e4a2c2da997596f2db6c9590f58d
Gabo <gabo@MacBook-Air-de-Gabo.local>
2025-11-13 18:48:03 -0600

    Agregue mi Tic-Tac-Toe a Github

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gchiv.github.io/github-pages' 
+  base: 'https://gchiv.github.io/Tic-Tac-Toe',
 })
